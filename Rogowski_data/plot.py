5e0375777d6e82fc1c8a9b725194f06c375cc9c7
Lala Shakti Swarup Ray <lalashakti95@gmail.com>
2024-07-29 16:17:09 +0200

    update

--- Rogowski_data/plot.py
+++ Rogowski_data/plot.py
@@ -2,14 +2,23 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
-file_path = '/Users/virginianegri/Desktop/Rogowski_data/40C/40C_350Hz_20A.csv'
-file_path = '/Users/virginianegri/Desktop/Rogowski_data/40C/40C_650Hz_20A.csv'
-# file_path = '/Users/virginianegri/Desktop/Rogowski_data/40C/40C_sinc_100A.csv'
+file_path = 'Rogowski_data/-15C_150Hz_10A.csv'
+file_path_alt = 'Rogowski_data/55C_150Hz_10A.csv'
+file_path_alt_2 = 'Rogowski_data/-15C_2500Hz_10A.csv'
+file_path_alt_3 = 'Rogowski_data/-15C_150Hz_20A.csv'
 
 data = pd.read_csv(file_path,sep=';',header=None)
-print(data)
+data_alt = pd.read_csv(file_path_alt,sep=';',header=None)
+data_alt_2 = pd.read_csv(file_path_alt_2,sep=';',header=None)
+data_alt_3 = pd.read_csv(file_path_alt_3,sep=';',header=None)
+
 data = data.T
+data_alt = data_alt.T
+data_alt_2 = data_alt_2.T
+data_alt_3 = data_alt_3.T
 
 data.iloc[0:999].plot(legend=True) #plot
-# data.iloc[130:230].plot(legend=True) #plot for the sinc
+data_alt.iloc[0:999].plot(legend=True) #plot
+data_alt_2.iloc[0:999].plot(legend=True) #plot
+data_alt_3.iloc[0:999].plot(legend=True) #plot
 plt.show()
